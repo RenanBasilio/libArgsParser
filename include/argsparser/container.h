87f5f8709e6a82c69cd57f5bc6ab38bf2cbe56d7
renanbasilio <renanbasilio@poli.ufrj.br>
2018-02-03 18:30:57 -0800

    Changed post-processor type callback to take a second void pointer as an optional argument.

--- include/argsparser/container.h
+++ include/argsparser/container.h
@@ -25,26 +25,12 @@ namespace ArgsParser
             // To-Do: Make C++17 version with std::optional support.
             std::pair<bool, std::string> getValue();
             std::pair<bool, std::string> getValidation();
-            std::pair<bool, void*>       getPostResult();
 
         private:
             const ContainerImpl* container_impl;
 
             friend class Parser;
 
-            /**
-             * Constructor for the Container structure.
-             */
-            Container(std::string name, 
-                char short_id, 
-                std::string long_id, 
-                std::string placeholder, 
-                std::string desc,
-                Validator validator,
-                bool validation_critical,
-                Callback callback)
-                {
-
-                }
+            std::tuple<bool, Callback, Postprocessor> getPostFunction();
     };
 }
